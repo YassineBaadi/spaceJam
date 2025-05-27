5076e8f18c442d972ca422f8d47b33aa6415fe37
Yassine <baadiyassine01@gmail.com>
2025-05-27 09:56:25 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-      
+      <Routes>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+      </Routes>
     </>
   )
 }
